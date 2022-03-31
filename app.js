38343e1ecae7a6b23df3d5c8f33da5ae3e53001b
Ngi Bujri <nbujri7@gmail.com>
2022-03-31 18:15:42 -0400

    added functions to validate grid size and generate the grid

---
+++ app.js
@@ -0,0 +1,24 @@
+const gridContainer = document.querySelector('.grid-container');
+
+// generate dynamic grid of divs based on two inputs
+function generateGrid(size) {
+    const gridSize = size * size;
+    for (let i = 0; i < gridSize; i++) {
+        const item = document.createElement('div');
+        item.className = 'item';
+        gridContainer.appendChild(item);
+    }
+    gridContainer.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`)
+}
+
+function checkSize(size) {
+    if (size < 4) {
+        alert("MINIMUM SIZE MUST BE 4!")
+    } else if (size > 100) {
+        alert("MAXIMUM SIZE IS 100!")
+    } else {
+        generateGrid(size)
+    }
+}
+
+checkSize(100)
