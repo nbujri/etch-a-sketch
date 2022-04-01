7d9eb82c1dc8b28de44c841f58a52a456f2a6e22
Ngi Bujri <nbujri7@gmail.com>
2022-04-01 17:52:02 -0400

    ability to change grid size with a slider added

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const gridContainer = document.querySelector('.grid-container');
 const drawBtn = document.querySelector('.draw-btn');
 const clearBtn = document.querySelector('.clear-btn');
 const eraseBtn = document.querySelector('.eraser-btn');
+const sizeSlider = document.querySelector('.size-slider');
+const sliderValue = document.querySelector('.slider-value');
+defaultSize = 16;
+sizeSlider.value = defaultSize;
 let drawActive = true;
 
+function generateGrid() {
+    setGridSize(defaultSize);
+}
+
 // generate dynamic grid of divs based on two inputs
-function generateGrid(size) {
+function setGridSize(size) {
     const gridSize = size * size;
     for (let i = 0; i < gridSize; i++) {
         const item = document.createElement('div');
@@ -17,16 +25,6 @@ function generateGrid(size) {
         );
 }
 
-function checkSize(size) {
-    if (size < 4) {
-        alert("MINIMUM SIZE MUST BE 4!");
-    } else if (size > 100) {
-        alert("MAXIMUM SIZE IS 100!");
-    } else {
-        generateGrid(size);
-    }
-}
-
 function draw(e) {
     if (drawActive) {
         e.target.classList.add('draw');
@@ -36,22 +34,50 @@ function draw(e) {
 }
 
 function clearGrid() {
+    const items = document.querySelectorAll('.item');
     items.forEach(item => item.classList.remove('draw'));
 }
 
-checkSize(16)
+function clearNode() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function updateGrid() {
+    clearGrid();
+    clearNode();
+    setGridSize(sizeSlider.value);
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => item.addEventListener('mouseover', draw));
+}
+
+generateGrid();
 
 const items = document.querySelectorAll('.item');
 
-items.forEach(item => item.addEventListener('mouseover', draw))
+items.forEach(item => item.addEventListener('mouseover', draw));
+
 clearBtn.addEventListener('click', clearGrid);
+
 drawBtn.addEventListener('click', function() {
     drawActive = true;
     console.log(drawActive);
-})
+});
+
 eraseBtn.addEventListener('click', function() {
     drawActive = false;
     console.log(drawActive);
-})
+});
+
+sizeSlider.addEventListener('input', function(e) {
+    sliderValue.textContent = sizeSlider.value;
+});
+
+sizeSlider.addEventListener('change', function() {
+    updateGrid();
+});
+
+sliderValue.textContent = sizeSlider.value;
 
 
