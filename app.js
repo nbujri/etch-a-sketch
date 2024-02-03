5ad79d3973c37bdc4b6f304860397ab888fd25d5
Ngi Bujri <nbujri7@gmail.com>
2024-02-03 12:43:34 -0500

    create function to generate grid

---
+++ app.js
@@ -0,0 +1,25 @@
+// make a 16x16 grid for default size
+// grid size can be changed
+// limit size to 100x100
+const grid = document.querySelector(".grid");
+let gridHeight = 16;
+let gridWidth = 16;
+let gridSize = gridHeight * gridWidth;
+
+function generateGrid(height, width) {
+  for (let h = 0; h < height; h++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    grid.appendChild(column);
+    for (let w = 0; w < width; w++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      column.appendChild(cell);
+    }
+  }
+}
+
+generateGrid(gridHeight, gridWidth);
+
+// when mouse cursor hovers over a cell
+// change the cell color
